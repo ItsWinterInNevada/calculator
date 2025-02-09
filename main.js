d474752ad4de42faaec06438361712cc2fc8383f
Ashley Winters <itswinterinnevada@mailbox.org>
2025-02-09 08:36:38 +0000

    Add feature to return array when = is pressed to allow continual calculation. Add reset feature

--- main.js
+++ main.js
@@ -1,26 +1,75 @@
-const numInput = document.querySelectorAll(`.num`);
-const evaluate = document.querySelector(`.equals`);
+const numInputBtn = document.querySelectorAll(`.num`);
+const plusBtn = document.querySelector(`.plus`);
+const minusBtn = document.querySelector(`.minus`);
+const multiplyBtn = document.querySelector(`.multiply`);
+const divideBtn = document.querySelector(`.divide`);
+const evaluateBtn = document.querySelector(`.equals`);
+const resetBtn = document.querySelector(`.reset`);
 
-const evaluateExpression = () => {
-  let input = [0];
+let input = [0]; // 0 fixes typeError from first result being undefined by default when reducing.
+let currentOperator = `+`;
 
-  numInput.forEach((num) => {
-    num.addEventListener(`click`, (event) => {
-      input.push(Number(event.target.textContent));
-      console.log(input);
-    });
-  });
-
-  evaluate.addEventListener(
-    `click`,
-    () => {
-      const result = input.reduce(
-        (accumulator, currentValue) => accumulator + currentValue
-      );
-      console.log(result);
-    },
-    0
-  );
+const operators = {
+  plus: `+`,
+  minus: `-`,
+  multiply: `*`,
+  divide: `/`,
 };
 
-evaluateExpression();
+resetBtn.addEventListener(`click`, () => {
+  reset();
+});
+
+const reset = () => {
+  input = [0];
+  currentOperator = null;
+  console.log(input, currentOperator);
+};
+
+const fetchInput = () => {
+  numInputBtn.forEach((num) => {
+    num.addEventListener(`click`, (event) => {
+      if (input.at(0) === 0) {
+        input.shift(0);
+        input.push(Number(event.target.textContent));
+        console.log(input);
+      } else {
+        input.push(Number(event.target.textContent));
+        console.log(input);
+      }
+    });
+  });
+};
+
+const evaluateExpression = ({ plus, minus, multiply, divide }) => {
+  plusBtn.addEventListener(`click`, () => (currentOperator = plus));
+  minusBtn.addEventListener(`click`, () => (currentOperator = minus));
+  multiplyBtn.addEventListener(`click`, () => (currentOperator = multiply));
+  divideBtn.addEventListener(`click`, () => (currentOperator = divide));
+
+  evaluateBtn.addEventListener(`click`, () => {
+    let result = input.reduce((accumulator, currentValue) => {
+      if (currentOperator === plus) {
+        return accumulator + currentValue;
+      }
+      if (currentOperator === minus) {
+        return accumulator - currentValue;
+      }
+      if (currentOperator === multiply) {
+        return accumulator * currentValue;
+      }
+      if (currentOperator === divide) {
+        return accumulator / currentValue;
+      }
+    });
+    console.log(result);
+
+    input = [result];
+    console.log(input);
+  });
+};
+
+console.log(input);
+
+fetchInput();
+evaluateExpression(operators);
